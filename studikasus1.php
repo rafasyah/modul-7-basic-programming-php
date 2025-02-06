02c1065d428cb0b892bbf8971be8b8a39926ba04
rafasyah <rmrainaya@gmail.com>
2025-02-06 15:27:11 +0700

    improved studikasus

--- studikasus1.php
+++ studikasus1.php
@@ -1,26 +1,33 @@
 <?php
 
-echo"studi kasus";
-echo "<hr>";
+$gaji = 16000000;
+$status = "kontrak";
 
-$gaji="13000000";
-$status="tetap";
-$persen= ($pajak / 100);
-$potongan= $gaji * $pajak;
+if ($gaji >= 15000000  ) {
+    $pajak = 20;
+} elseif ( $gaji >= 10000000 && $gaji <= 15000000  ) {
+    $pajak = 15;
+} elseif ( $gaji >= 5000000 && $gaji <= 10000000  ) {
+    $pajak = 10;
+} elseif ( $gaji < 5000000 ) {
+    $pajak = 5;
+}
 
-  if ($gaji >= 15000000) {
-        $pajak = 20;
-    } elseif ($gaji >= 10000000) {
-        $pajak = 15;
-    } elseif ($gaji >= 5000000) {
-        $pajak = 10;
-    } else {
-        $pajak = 5;
-    }
+if ($status == "tetap") {
+    $pajak = $pajak + 5 ;
+}else {
+    $pajak = $pajak + 0;
+}
 
-    echo "-----rincian pajak----";
-    echo "Gaji Bulanan $gaji";
+$persenan = $pajak / 100 ;
+$potongan = $gaji * $persenan;
+$gaji_bersih = $gaji - $potongan;
 
+echo "Gaji Bulanan = Rp. " .$gaji . "<br>";
+echo "Status Pegawai =" . $status .  "<br>";
+echo "Pajak = " . $pajak ."%<br>";
+echo "Potongan Pajak =Rp. " .$potongan . "<br>" ;
+echo "Gaji Bersih =Rp. " . $gaji_bersih . "<br>";
        
     
 
