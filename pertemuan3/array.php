3282e78a4f68c5725f6a3ed13686fc6435d7f16b
rafasyah <rmrainaya@gmail.com>
2025-02-13 10:12:58 +0700

    stuff

---
+++ pertemuan3/array.php
@@ -0,0 +1,23 @@
+<!-- array -->
+
+<?php
+
+// definisi array
+$produk= ["laptop","mouse","keyboard"];
+echo $produk[0]. "<br>";
+echo $produk[1]. "<br>";
+echo $produk[2]. "<br>";
+
+// array asossiatif
+$buah = [
+    "nama" => "apel",
+    "warna" => "hijau",
+    "rasa" => "asam"
+
+    
+    
+];
+
+echo "nama buah" . $buah ['nama'] ."<br>";
+echo "warna buah" . $buah ['warna'] ."<br>";
+echo "rasa buah". $buah ['rasa'] ."<br>";
---
+++ pertemuan3/pengulangan.php
@@ -0,0 +1,14 @@
+<?php
+
+// for ($i=1; $i <= 10; $i++) { 
+//     echo "loop ke $i<br>";
+// }
+// echo "pengulangan decrement<br>";
+
+// for ($j=10; $j >= 1; $j--) { 
+//     echo "$j <br>";
+// }
+
+for ($i=1; $i <= 5; $i++) { 
+    echo "10 x $i =  ". 10 * $i . "<br>";
+}
